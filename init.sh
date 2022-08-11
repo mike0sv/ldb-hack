0bb2aa5dc5c97c49b317339670128cd1aa5dc25c
omesser <odedmesser@gmail.com>
2022-08-11 22:15:47 +0300

    adding kaggle chi vs muff and standford

--- init.sh
+++ init.sh
@@ -1,2 +1,5 @@
 ldb init .ldb
 ldb add-storage s3://ldb-public/remote/data-lakes/ -o anon true
+ldb add-storage s3://ldb-hackathon-team-6/clean-up
+ldb add-storage s3://ldb-hackathon-team-6/domas
+ldb add-storage s3://ldb-hackathon-team-6/data-lakes/oded
--- reproduce.sh
+++ reproduce.sh
@@ -3,18 +3,34 @@ set -exu
 export LDB_DIR=.ldb
 
 
-ldb index s3://ldb-hackathon-team-6/clean-up/train --format annot -p single-file=true --add-tag train
-ldb index s3://ldb-hackathon-team-6/clean-up/val --format annot -p single-file=true --add-tag val
-ldb index s3://ldb-hackathon-team-6/clean-up/labelbook --format annot -p single-file=true --add-tag labelbook
-ldb index s3://ldb-public/remote/data-lakes/chihuahua_vs_muffin/ --format infer --add-tag kaggle,train
+# starter baseline - but cleaned
+AWS_PROFILE=hackathon-team-6 ldb index s3://ldb-hackathon-team-6/clean-up/train --format annot -p single-file=true
+AWS_PROFILE=hackathon-team-6 ldb index s3://ldb-hackathon-team-6/clean-up/val --format annot -p single-file=true
+AWS_PROFILE=hackathon-team-6 ldb index s3://ldb-hackathon-team-6/clean-up/labelbook --format annot -p single-file=true
 
-ldb index s3://ldb-public/remote/data-lakes/Stanford-dog-breeds/n02109525-Saint_Bernard/ --format infer --param base-label=dog --add-tag stanford,train
+# personal images
+AWS_PROFILE=hackathon-team-6 ldb index s3://ldb-hackathon-team-6/domas --format infer --add-tag domas
+AWS_PROFILE=hackathon-team-6 ldb index s3://ldb-hackathon-team-6/data-lakes/oded --format infer --add-tag oded
 
+# kaggle - chihuahua vs muffin: correctly labeled - dog (279) and muffin (273)
+ldb index s3://ldb-public/remote/data-lakes/chihuahua_vs_muffin/ --format infer --add-tag kaggle,chihuahua_vs_muffin
+
+# stanford - labeled by dog breads (10800 objects, check out stanford_breeds_count.txt for counts)
+# I picked some large breeds in random
+# TODO - add the entire dataset?
+ldb index s3://ldb-public/remote/data-lakes/Stanford-dog-breeds/n02109525-Saint_Bernard/ --format infer --param base-label=dog --add-tag stanford
+ldb index s3://ldb-public/remote/data-lakes/Stanford-dog-breeds/n02091831-Saluki/ --format infer --param base-label=dog --add-tag stanford
+ldb index s3://ldb-public/remote/data-lakes/Stanford-dog-breeds/n02093256-Staffordshire_bullterrier/ --format infer --param base-label=dog --add-tag stanford
+ldb index s3://ldb-public/remote/data-lakes/Stanford-dog-breeds/n02110185-Siberian_husky/ --format infer --param base-label=dog --add-tag stanford
+ldb index s3://ldb-public/remote/data-lakes/Stanford-dog-breeds/n02092339-Weimaraner/ --format infer --param base-label=dog --add-tag stanford
+
+# those maybe similar to cats
+ldb index s3://ldb-public/remote/data-lakes/Stanford-dog-breeds/n02086910-papillon/ --format infer --param base-label=dog --add-tag stanford
+ldb index s3://ldb-public/remote/data-lakes/Stanford-dog-breeds/n02115913-dhole/ --format infer --param base-label=dog --add-tag stanford
+
+# google open images
 ldb index google_prepared/ --format annot -p single-file=true --add-tag train
 
 ldb eval -j --query label ds:root | sort | uniq -c
 
 python reset_and_resplit.py
-python sort_dataset.py dataset/train/dataset.json
-python sort_dataset.py dataset/val/dataset.json
-python sort_dataset.py dataset/labelbook/dataset.json
