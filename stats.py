167b7c9d977e404adb16e74a54c21922be10e505
mike0sv <mike0sv@gmail.com>
2022-08-12 12:31:58 +0300

    formatting for stats.py

--- stats.py
+++ stats.py
@@ -13,11 +13,13 @@ DATASETS = {
     "s3://ldb-public/remote/data-lakes/Stanford-dog-breeds": "stanford"
 }
 
+
 def get_dataset(path):
     for d in DATASETS:
         if path.startswith(d):
             return DATASETS[d]
     return path
+
 
 def main():
     all_hashes = Counter()
@@ -27,14 +29,18 @@ def main():
 
             print(split, len(data))
             counts = Counter(o["annotation"]["label"] for o in data)
+            print("Classes:")
             print("\n".join(
-                f"{k}: {v / len(data) * 100:.2f}" for k, v in counts.items()))
+                f"{k}: {v / len(data) * 100:.2f}%" for k, v in counts.items()))
             print()
 
-            datasets = Counter(get_dataset(o["data-object-info"]["path"]) for o in data)
+            datasets = Counter(
+                get_dataset(o["data-object-info"]["path"]) for o in data)
+            print("Datasets:")
             print("\n".join(
-                f"{k}: {v / len(data) * 100:.2f}" for k, v in datasets.items()))
-            print()
+                f"{k}: {v / len(data) * 100:.2f}%" for k, v in
+                datasets.items()))
+            print("-" * 20)
             print()
 
             all_hashes.update(o["data-object-info"]["md5"] for o in data)
